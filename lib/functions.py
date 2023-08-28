c72219ea96b4032f3d63a0fccae9c182bc93b1a6
Wycliffe <wycliffekiprono4@gmail.com>
2023-08-28 17:49:02 +0300

    python functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None 
+    
+    return number / 2
+
+
+# function myFunction(param) {
+#     console.log("Running myFunction")
+#     return param + 1
+# }
+
+# myFunction(1)
+
+def my_function(param):  #use the def keyword to identify this code as a function
+    print("Running my_function")
+    return param + 1
+my_function_return_value = my_function(1)
+print(my_function_return_value)
